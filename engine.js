79b5bc476c26c2b341be6f1a6d8b9f0ed55850bd
thejupitergroup <ourjupitergroup@gmail.com>
2025-03-16 10:33:05 -0400

    Alpha 0.2

--- engine.js
+++ engine.js
@@ -1,19 +1,23 @@
+// Function, well, loads the desired page in the iFrame...
 function loadPage() {
     let input = document.getElementById("urlInput").value;
     let frame = document.getElementById("browserFrame");
 
+    // Determins whether the text in the input is meant to be a query or a URL.
     if (input.includes(".") && !input.includes(" ")) {
         if (!input.startsWith("http")) {
             input = "https://" + input;
         }
         
-        // Use AllOrigins proxy to bypass X-Frame-Options
-        frame.src = "https://barclaycors.ourjupitergroup.workers.dev/?url=" + encodeURIComponent(input);
+        // This is our WebViewer engine, which is a CORS proxy so many websites can load.
+        frame.src = "https://webviewer.ourjupitergroup.workers.dev/?src=" + encodeURIComponent(input);
     } else {
-        frame.src = "https://barclaywebtech.pages.dev/search#gsc.tab=0&gsc.q=" + encodeURIComponent(input) + "&gsc.sort=";
+        // Other queries load in a CSE.
+        frame.src = "https://barclaygo.pages.dev/results?q=" + encodeURIComponent(input) + "#gsc.tab=0&gsc.q=" +encodeURIComponent(input) + "&gsc.page=1";
     }
 }
 
+// Lets the user press "enter" instead of clicking the button.
 function handleKeyPress(event) {
     if (event.key === "Enter") {
         loadPage();
