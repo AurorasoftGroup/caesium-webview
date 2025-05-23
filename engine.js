6face36c9087e944eeee954c4ea00091de45bf21
thejupitergroup <ourjupitergroup@gmail.com>
2025-05-22 21:51:35 -0400

    Alpha 0.5

--- engine.js
+++ engine.js
@@ -1,26 +1,17 @@
-// Function, well, loads the desired page in the iFrame...
 function loadPage() {
     let input = document.getElementById("urlInput").value;
     let frame = document.getElementById("browserFrame");
 
-    // Determins whether the text in the input is meant to be a query or a URL.
     if (input.includes(".") && !input.includes(" ")) {
-        if (!input.startsWith("www.")) {
-            input = "www." + input;
+        if (!input.startsWith("http")) {
+            input = "https://" + input;
         }
-        if (input.startsWith("http")) {
-            input = '';
-        }
-        
-        // This is our WebViewer engine, which is a CORS proxy so many websites can load.
-        frame.src = "https://webviewer.ourjupitergroup.workers.dev/?url=" + encodeURIComponent(input);
+        frame.src = "https://corsproxy.ourjupitergroup.workers.dev/?url=" + encodeURIComponent(input);
     } else {
-        // Other queries load in a CSE.
-        frame.src = "https://barclaygo.pages.dev/results?q=" + encodeURIComponent(input) + "#gsc.tab=0&gsc.q=" +encodeURIComponent(input) + "&gsc.page=1";
+        frame.src = "https://barclaywebtech.pages.dev/search#gsc.tab=0&gsc.q=" + encodeURIComponent(input) + "&gsc.sort=";
     }
 }
 
-// Lets the user press "enter" instead of clicking the button.
 function handleKeyPress(event) {
     if (event.key === "Enter") {
         loadPage();
